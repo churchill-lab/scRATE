f79444cecc3f087b898bfbe582654c4e2bab19e3
KB <kb.choi@jax.org>
2020-06-03 21:28:11 -0400

    [kbchoi] Added create_loom function

--- R/create_loom.R
+++ R/create_loom.R
@@ -8,11 +8,16 @@
 #'
 create_loom <- function(efile, outfile, min_expressed_cells=0) {
   cntmat <- read.table(efile, row.names=1, header=TRUE, check.names=FALSE)
+  message(sprintf('[scRATE::create_loom] Loaded a count matrix of %d genes x %d cells', dim(cntmat)[1], dim(cntmat)[2]))
   binmat <- cntmat > 0
   num_expr <- rowSums(binmat)
+  selected <- num_expr > min_expressed_cells
+  message(sprintf('[scRATE::create_loom] There were %d genes that pass the minimum expressed cell count requirement', sum(selected)))
+  message(sprintf('[scRATE::create_loom] Creating a loom file'))
   loomR::create(filename = outfile,
                 data = cntmat,
                 feature.attrs = list(GeneID=rownames(cntmat),
-                                     Selected=num_expr > min_expressed_cells),
+                                     Selected=selected),
                 cell.attrs = list(Size=colSums(cntmat)))
+  message(sprintf('[scRATE::create_loom] Finished creating a loom file, %s.', outfile))
 }
